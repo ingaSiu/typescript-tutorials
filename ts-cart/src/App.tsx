b0421643dc280b956b943330ade27e8cb64a0279
ingaSiu <ingasiud@gmail.com>
2023-03-05 18:51:16 +0200

    page

--- ts-cart/src/App.tsx
+++ ts-cart/src/App.tsx
@@ -1,5 +1,23 @@
+import Cart from './components/Cart';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import { useState } from 'react';
+
 const App = () => {
-  return <div className="App"></div>;
+  const [viewCart, setViewCart] = useState<boolean>(false);
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />;
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
+  );
+
+  return content;
 };
 
 export default App;
