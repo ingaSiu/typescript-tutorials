0f302ba8f85c1f7229019d1ed18dcfb1eceee824
ingaSiu <ingasiud@gmail.com>
2023-03-06 09:23:28 +0200

    Cart line item component

--- ts-cart/src/components/Cart.tsx
+++ ts-cart/src/components/Cart.tsx
@@ -1,5 +1,40 @@
+import CartLineItem from './CartLineItem';
+import useCart from '../hooks/useCart';
+import { useState } from 'react';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const [confirm, setConfirm] = useState<boolean>(false);
+  const { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart } = useCart();
+
+  const onSubmitOrder = () => {
+    dispatch({ type: REDUCER_ACTIONS.SUBMIT });
+    setConfirm(true);
+  };
+
+  const pageContent = confirm ? (
+    <h2>Tahnk you for your order.</h2>
+  ) : (
+    <>
+      <h2 className="offscreen">Cart</h2>
+      <ul className="cart">
+        {cart.map((item) => {
+          return <CartLineItem key={item.sku} item={item} dispatch={dispatch} REDUCER_ACTIONS={REDUCER_ACTIONS} />;
+        })}
+      </ul>
+
+      <div className="cart__totals">
+        <p>Total items: {totalItems}</p>
+        <p>Total price: {totalPrice}</p>
+        <button className="cart__submit" disabled={!totalItems} onClick={onSubmitOrder}>
+          Place Order
+        </button>
+      </div>
+    </>
+  );
+
+  const content = <main className="main main--cart">{pageContent}</main>;
+
+  return content;
 };
 
 export default Cart;
