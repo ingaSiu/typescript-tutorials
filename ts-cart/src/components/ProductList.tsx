5e0ed42bc50cb1f56b568695ed02c3e1db51bdc8
ingaSiu <ingasiud@gmail.com>
2023-03-06 09:24:06 +0200

    Products list component

--- ts-cart/src/components/ProductList.tsx
+++ ts-cart/src/components/ProductList.tsx
@@ -1,5 +1,35 @@
+import CartContext from '../context/CartProvider';
+import Product from './Product';
+import { ReactElement } from 'react';
+import { UseCartContextType } from '../context/CartProvider';
+import useCart from '../hooks/useCart';
+import useProducts from '../hooks/useProducts';
+
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const { dispatch, REDUCER_ACTIONS, cart } = useCart();
+  const { products } = useProducts();
+
+  let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>;
+
+  if (products?.length) {
+    pageContent = products.map((product) => {
+      const inCart: boolean = cart.some((item) => item.sku === product.sku);
+
+      return (
+        <Product
+          key={product.sku}
+          product={product}
+          dispatch={dispatch}
+          REDUCER_ACTIONS={REDUCER_ACTIONS}
+          inCart={inCart}
+        />
+      );
+    });
+  }
+
+  const content = <main className="main main--products">{pageContent}</main>;
+
+  return content;
 };
 
 export default ProductList;
