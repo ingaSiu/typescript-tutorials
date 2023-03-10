b5536a2d30038f229fa1b239abe2a8a7e52b4ba9
ingaSiu <ingasiud@gmail.com>
2023-03-10 17:31:47 +0200

    store page

--- ts-cart2/src/pages/Store.tsx
+++ ts-cart2/src/pages/Store.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+import StoreItem from '../components/StoreItem';
+import storeItems from '../data/items.json';
 
 type Props = {};
 
 const Store = (props: Props) => {
-  return <div>Store</div>;
+  return (
+    <>
+      <div>Store</div>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Store;
