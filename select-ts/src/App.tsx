c0fe8371e69ec76dd45f71094581f54c919d32ac
ingaSiu <ingasiud@gmail.com>
2023-03-19 20:17:33 +0200

    added select component

---
+++ select-ts/src/App.tsx
@@ -0,0 +1,23 @@
+import { Select } from './components/Select';
+import { useState } from 'react';
+
+const options = [
+  { label: 'First', value: 1 },
+  { label: 'Second', value: 2 },
+  { label: 'Third', value: 3 },
+  { label: 'Fourth', value: 4 },
+  { label: 'Fifth', value: 5 },
+];
+
+const App = () => {
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+
+  return (
+    <>
+      <Select options={options} value={value} onChange={(o) => setValue(o)} />
+    </>
+  );
+};
+
+export default App;
+
