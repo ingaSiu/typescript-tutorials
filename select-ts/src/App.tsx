0bab285bffa3e2ce54685cb5a924584012dbc0b0
ingaSiu <ingasiud@gmail.com>
2023-03-20 08:57:23 +0200

    added multiple choice component

--- select-ts/src/App.tsx
+++ select-ts/src/App.tsx
@@ -1,4 +1,5 @@
-import { Select } from './components/Select';
+import { Select, SelectOption } from './components/Select';
+
 import { useState } from 'react';
 
 const options = [
@@ -10,11 +11,14 @@ const options = [
 ];
 
 const App = () => {
-  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+  const [value1, setValue1] = useState<SelectOption[] | undefined>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
 
   return (
     <>
-      <Select options={options} value={value} onChange={(o) => setValue(o)} />
+      <Select multiple options={options} value={value1} onChange={(o) => setValue1(o)} />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
     </>
   );
 };
