9bf692ed5dd8ea618fcc55375a7cb6a7cd6d0c81
ingaSiu <ingasiud@gmail.com>
2023-10-26 16:30:11 +0300

    add page header component

--- youtube-home-page/src/App.tsx
+++ youtube-home-page/src/App.tsx
@@ -1,5 +1,12 @@
+import PageHeader from './components/PageHeader';
+
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div>2</div>
+    </div>
+  );
 };
 
 export default App;
