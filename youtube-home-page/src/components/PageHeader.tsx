17bff5344346f9a08e0ae5925c76ed9595310379
ingaSiu <ingasiud@gmail.com>
2023-10-28 08:17:56 +0300

    remove unnecessary text

--- youtube-home-page/src/components/PageHeader.tsx
+++ youtube-home-page/src/components/PageHeader.tsx
@@ -6,7 +6,7 @@ const PageHeader = () => {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <Button variant="ghost" size="icon" className="">
+        <Button variant="ghost" size="icon">
           <Menu />
         </Button>
         <a href="/">
