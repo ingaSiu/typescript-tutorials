9b01c3ba3f529103ffb441909a995fa6b63b6a3a
ingaSiu <ingasiud@gmail.com>
2023-10-28 08:17:15 +0300

    make a custom btn

--- youtube-home-page/src/components/Button.tsx
+++ youtube-home-page/src/components/Button.tsx
@@ -1,25 +1,31 @@
-import { cva } from 'class-variance-authority';
+import { VariantProps, cva } from 'class-variance-authority';
 
-const buttonStyles = cva([
-  'transition-colors',
-  {
-    variants: {
-      variant: {
-        default: ['bg-secondary', 'hover:bg-secondary-hover'],
-        ghost: ['hover:bg-gray-100'],
-      },
-      size: {
-        dafault: ['rounded', 'p-2'],
-        icon: ['rounded-full', 'w-10', 'h-10', 'flex', 'items-center', 'justify-center', 'p-2.5'],
-      },
+import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+const buttonStyles = cva(['transition-colors'], {
+  variants: {
+    variant: {
+      default: ['bg-secondary', 'hover:bg-secondary-hover'],
+      ghost: ['hover:bg-gray-100'],
+    },
+    size: {
+      default: ['rounded', 'p-2'],
+      icon: ['rounded-full', 'w-10', 'h-10', 'flex', 'items-center', 'justify-center', 'p-2.5'],
     },
   },
-]);
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+});
 
-const classes = buttonStyles({ size: 'icon' });
+// const classes = buttonStyles({ size: 'icon' });
 
-const Button = () => {
-  return <button />;
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<'button'>;
+
+const Button = ({ variant, size, className, ...props }: ButtonProps) => {
+  return <button {...props} className={twMerge(buttonStyles({ variant, size }), className)} />;
 };
 
 export default Button;
