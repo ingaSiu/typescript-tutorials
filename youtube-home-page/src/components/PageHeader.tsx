78ebf225b9e3d52ae8723f7a3aaad3189387cb42
ingaSiu <ingasiud@gmail.com>
2023-10-28 08:17:38 +0300

    add proper btn component

--- youtube-home-page/src/components/PageHeader.tsx
+++ youtube-home-page/src/components/PageHeader.tsx
@@ -6,7 +6,7 @@ const PageHeader = () => {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <Button>
+        <Button variant="ghost" size="icon" className="">
           <Menu />
         </Button>
         <a href="/">
