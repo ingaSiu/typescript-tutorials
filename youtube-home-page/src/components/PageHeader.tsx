3450167b8d0490d09e00c1dcfabf0a779cf14525
ingaSiu <ingasiud@gmail.com>
2023-10-29 07:22:32 +0200

    add right side icons

--- youtube-home-page/src/components/PageHeader.tsx
+++ youtube-home-page/src/components/PageHeader.tsx
@@ -1,10 +1,11 @@
+import { Bell, Menu, Upload, User } from 'lucide-react';
+
 import Button from './Button';
-import { Menu } from 'lucide-react';
 import logo from '../assets/Logo.png';
 
 const PageHeader = () => {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
       <div className="flex gap-4 items-center flex-shrink-0">
         <Button variant="ghost" size="icon">
           <Menu />
@@ -14,7 +15,17 @@ const PageHeader = () => {
         </a>
       </div>
       <div></div>
-      <div></div>
+      <div className="flex flex-shrink-0 md:gap-2 ">
+        <Button size="icon" variant="ghost">
+          <Upload />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User />
+        </Button>
+      </div>
     </div>
   );
 };
