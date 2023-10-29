89a582d7decba83e833a41ba79dd5f3163448270
ingaSiu <ingasiud@gmail.com>
2023-10-29 08:02:27 +0200

    display search bar onClick

--- youtube-home-page/src/components/PageHeader.tsx
+++ youtube-home-page/src/components/PageHeader.tsx
@@ -2,11 +2,14 @@ import { Bell, Menu, Mic, Search, Upload, User } from 'lucide-react';
 
 import Button from './Button';
 import logo from '../assets/Logo.png';
+import { useState } from 'react';
 
 const PageHeader = () => {
+  const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
+
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-      <div className="flex gap-4 items-center flex-shrink-0">
+      <div className={`gap-4 items-center flex-shrink-0 ${showFullWidthSearch ? 'hidden' : 'flex'}`}>
         <Button variant="ghost" size="icon">
           <Menu />
         </Button>
@@ -14,7 +17,7 @@ const PageHeader = () => {
           <img src={logo} className="h-6" />
         </a>
       </div>
-      <form className="md:flex hidden gap-4 flex-grow justify-center">
+      <form className={` gap-4 flex-grow justify-center ${showFullWidthSearch ? 'flex' : 'hidden md:flex'}`}>
         <div className="flex flex-grow max-w-[600px]">
           <input
             type="search"
@@ -30,8 +33,8 @@ const PageHeader = () => {
           <Mic />
         </Button>
       </form>
-      <div className="flex flex-shrink-0 md:gap-2 ">
-        <Button size="icon" variant="ghost" className="md:hidden">
+      <div className={`flex-shrink-0 md:gap-2 ${showFullWidthSearch ? 'hidden' : 'flex'} `}>
+        <Button onClick={() => setShowFullWidthSearch(true)} size="icon" variant="ghost" className="md:hidden">
           <Search />
         </Button>
         <Button size="icon" variant="ghost" className="md:hidden">
