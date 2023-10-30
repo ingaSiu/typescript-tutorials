11c1a51c0629e06a285a81b028a7308a8fabcdb5
ingaSiu <ingasiud@gmail.com>
2023-10-30 09:13:12 +0200

    add scroll to side btn for categories

--- youtube-home-page/src/components/CategoryPills.tsx
+++ youtube-home-page/src/components/CategoryPills.tsx
@@ -1,15 +1,25 @@
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
 import Button from './Button';
+import { useState } from 'react';
 
 type CategoryPillProps = {
   categories: string[];
   selectedCategory: string;
   onSelect: (category: string) => void;
 };
+const TRANSLATE_AMOUNT = 200;
 
 const CategoryPills = ({ categories, selectedCategory, onSelect }: CategoryPillProps) => {
+  const [translate, setTranslate] = useState(0);
+  const [isLeftVisible, setIsLeftVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(false);
   return (
     <div className="overflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+      <div
+        className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map((category) => (
           <Button
             key={category}
@@ -21,6 +31,31 @@ const CategoryPills = ({ categories, selectedCategory, onSelect }: CategoryPillP
           </Button>
         ))}
       </div>
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            onClick={() => {
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_AMOUNT;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              });
+            }}
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+          <Button variant="ghost" size="icon" className="h-full aspect-square w-auto p-1.5">
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
