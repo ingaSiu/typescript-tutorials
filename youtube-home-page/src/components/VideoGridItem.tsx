e640b68331c17a8970f9d5d62d378171606054c7
ingaSiu <ingasiud@gmail.com>
2023-11-03 14:36:21 +0200

    add card elements

--- youtube-home-page/src/components/VideoGridItem.tsx
+++ youtube-home-page/src/components/VideoGridItem.tsx
@@ -1,4 +1,5 @@
 import { formatDuration } from '../utils/formatDuration';
+import { formatTimeAgo } from '../utils/formatTimeAgo';
 
 type VideoGridItemProps = {
   id: string;
@@ -14,6 +15,8 @@ type VideoGridItemProps = {
   thumbnailUrl: string;
   videoUrl: string;
 };
+
+const VIEW_FORMATTER = new Intl.NumberFormat(undefined, { notation: 'compact' });
 
 const VideoGridItem = ({
   id,
@@ -33,6 +36,22 @@ const VideoGridItem = ({
           {formatDuration(duration)}
         </div>
       </a>
+      <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+          <img className="w-12 h-12 rounded-full" src={channel.profileUrl} />
+        </a>
+        <div className="flex flex-col">
+          <a href={`/watch?v={id}`} className="font-bold">
+            {title}
+          </a>
+          <a href={`/@${channel.id}`} className="text-secondary-text text-sm">
+            {channel.name}
+          </a>
+          <div className="text-secondary-text text-sm">
+            {VIEW_FORMATTER.format(views)} Views • {formatTimeAgo(postedAt)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
