ef37e1031ac1346fd09c4212e09152b1bf82fdbc
ingaSiu <ingasiud@gmail.com>
2023-11-03 11:10:14 +0200

    create video item component

---
+++ youtube-home-page/src/components/VideoGridItem.tsx
@@ -0,0 +1,29 @@
+type VideoGridItemProps = {
+  id: string;
+  title: string;
+  channel: {
+    id: string;
+    name: string;
+    profileUrl: string;
+  };
+  views: number;
+  postedAt: Date;
+  duration: number;
+  thumbnailUrl: string;
+  videoUrl: string;
+};
+
+const VideoGridItem = ({
+  id,
+  title,
+  channel,
+  views,
+  postedAt,
+  duration,
+  thumbnailUrl,
+  videoUrl,
+}: VideoGridItemProps) => {
+  return <div className="flex flex-col gap-2"></div>;
+};
+
+export default VideoGridItem;
