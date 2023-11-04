d414413b36bf691d945a0d4bfa05ddd2a7d94968
ingaSiu <ingasiud@gmail.com>
2023-11-04 17:59:41 +0200

    import sidebar component

--- youtube-home-page/src/App.tsx
+++ youtube-home-page/src/App.tsx
@@ -2,6 +2,7 @@ import { categories, videos } from './data/home';
 
 import CategoryPills from './components/CategoryPills';
 import PageHeader from './components/PageHeader';
+import Sidebar from './components/Sidebar';
 import VideoGridItem from './components/VideoGridItem';
 import { useState } from 'react';
 
@@ -11,7 +12,7 @@ const App = () => {
     <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <div>Sidebar</div>
+        <Sidebar />
         <div className="overflow-x-hidden px-8 pb-4">
           <div className="sticky top-0 bg-white z-10 pb-4">
             <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
