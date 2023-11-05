2ab31261b24dbefea3198d35588cfc100036e635
ingaSiu <ingasiud@gmail.com>
2023-11-05 15:55:52 +0200

    add small sidebar with icons

--- youtube-home-page/src/components/Sidebar.tsx
+++ youtube-home-page/src/components/Sidebar.tsx
@@ -1,10 +1,16 @@
+import { Clapperboard, Home, Library, Repeat } from 'lucide-react';
+
 import { ElementType } from 'react';
-import { Home } from 'lucide-react';
+import { buttonStyles } from './Button';
+import { twMerge } from 'tailwind-merge';
 
 const Sidebar = () => {
   return (
     <aside className="sticky top-0 overflow-y-auto scrollball-hidden pb-4 flex flex-col ml-1 lg:hidden">
       <SmallSidebarItem Icon={Home} title="Home" url="/" />
+      <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+      <SmallSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
+      <SmallSidebarItem Icon={Library} title="Library" url="/library" />
     </aside>
   );
 };
@@ -19,7 +25,10 @@ type SmallSidebarItemProps = {
 
 const SmallSidebarItem = ({ Icon, title, url }: SmallSidebarItemProps) => {
   return (
-    <a href={url} className="">
+    <a
+      href={url}
+      className={twMerge(buttonStyles({ variant: 'ghost' }), 'py-4 px-1 flex flex-col items-center rouded-lg gap-1')}
+    >
       <Icon className="w-6 h-6" />
       <div className="text-sm">{title}</div>
     </a>
