65ef6794293ce5dedc42089982f5f3608a85917f
ingaSiu <ingasiud@gmail.com>
2023-11-05 17:05:16 +0200

    add second part of large sidebar

--- youtube-home-page/src/components/Sidebar.tsx
+++ youtube-home-page/src/components/Sidebar.tsx
@@ -1,7 +1,19 @@
 import Button, { buttonStyles } from './Button';
-import { ChevronDown, ChevronUp, Clapperboard, Home, Library, Repeat } from 'lucide-react';
+import {
+  ChevronDown,
+  ChevronUp,
+  Clapperboard,
+  Clock,
+  History,
+  Home,
+  Library,
+  ListVideo,
+  PlaySquare,
+  Repeat,
+} from 'lucide-react';
 import { Children, ElementType, ReactNode, useState } from 'react';
 
+import { playlists } from '../data/sidebar';
 import { twMerge } from 'tailwind-merge';
 
 const Sidebar = () => {
@@ -14,10 +26,26 @@ const Sidebar = () => {
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
       <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
-        <LargeSidebarSection visibleItemCount={1}>
+        <LargeSidebarSection>
           <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
-          <LargeSidebarItem Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
         </LargeSidebarSection>
+        <hr />
+        <LargeSidebarSection visibleItemCount={5}>
+          <LargeSidebarItem Icon={Library} title="Library" url="/library" />
+          <LargeSidebarItem Icon={History} title="History" url="/history" />
+          <LargeSidebarItem Icon={PlaySquare} title="Your Videos" url="/your-videos" />
+          <LargeSidebarItem Icon={Clock} title="Watch Later" url="/playlist?list=WL" />
+          {playlists.map((playlist) => (
+            <LargeSidebarItem
+              key={playlist.id}
+              Icon={ListVideo}
+              title={playlist.name}
+              url={`/playlist?list=WL${playlist.id}`}
+            />
+          ))}
+        </LargeSidebarSection>
+        <hr />
       </aside>
     </>
   );
