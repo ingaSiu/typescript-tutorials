d9791f83dee5e13f3eed6767fb8e1cd290b23eb3
ingaSiu <ingasiud@gmail.com>
2023-11-06 10:30:58 +0200

    create sidebar context

---
+++ youtube-home-page/src/contexts/SidebarContext.tsx
@@ -0,0 +1,51 @@
+import { ReactNode, createContext, useContext, useState } from 'react';
+
+type SidebarProviderProps = {
+  children: ReactNode;
+};
+
+type SidebarContextType = {
+  isLargeOpen: boolean;
+  isSmallOpen: boolean;
+  toggle: () => void;
+  close: () => void;
+};
+
+const SidebarContext = createContext<SidebarContextType | null>(null);
+
+// this allows to use our context in other places
+export const useSidebarContext = () => {
+  const value = useContext(SidebarContext);
+  if (value == null) throw Error('Cannot use outside of SidebarProvider');
+
+  return value;
+};
+
+export const SidebarProvider = ({ children }: SidebarProviderProps) => {
+  const [isLargeOpen, setIsLargeOpen] = useState(true);
+  const [isSmallOpen, setIsSmallOpen] = useState(false);
+
+  const isScreenSmall = () => {
+    return window.innerWidth < 1024;
+  };
+
+  const toggle = () => {
+    if (isScreenSmall()) {
+      setIsSmallOpen((s) => !s);
+    } else {
+      setIsLargeOpen((l) => !l);
+    }
+  };
+
+  const close = () => {
+    if (isScreenSmall()) {
+      setIsSmallOpen(false);
+    } else {
+      setIsLargeOpen(false);
+    }
+  };
+
+  return (
+    <SidebarContext.Provider value={{ isLargeOpen, isSmallOpen, toggle, close }}>{children}</SidebarContext.Provider>
+  );
+};
