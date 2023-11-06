4b7e1cb3beea614a3860d83c2dfda6fbd6d41d95
ingaSiu <ingasiud@gmail.com>
2023-11-06 11:29:09 +0200

    fix grid

--- youtube-home-page/src/App.tsx
+++ youtube-home-page/src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
                 onSelect={setSelectedCategory}
               />
             </div>
-            <div className="grid gap-4 grid-cols-[repeat(auto-fill, minmax(300px, 1fr))]">
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
               {videos.map((video) => (
                 <VideoGridItem key={video.id} {...video} />
               ))}
