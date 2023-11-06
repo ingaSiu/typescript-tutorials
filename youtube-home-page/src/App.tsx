d548747122df777a0aac6ba61486a2eb5e4720af
ingaSiu <ingasiud@gmail.com>
2023-11-06 10:32:03 +0200

    wrap app with sidebar context

--- youtube-home-page/src/App.tsx
+++ youtube-home-page/src/App.tsx
@@ -3,28 +3,35 @@ import { categories, videos } from './data/home';
 import CategoryPills from './components/CategoryPills';
 import PageHeader from './components/PageHeader';
 import Sidebar from './components/Sidebar';
+import { SidebarProvider } from './contexts/SidebarContext';
 import VideoGridItem from './components/VideoGridItem';
 import { useState } from 'react';
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   return (
-    <div className="max-h-screen flex flex-col">
-      <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <Sidebar />
-        <div className="overflow-x-hidden px-8 pb-4">
-          <div className="sticky top-0 bg-white z-10 pb-4">
-            <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
-          </div>
-          <div className="grid gap-4 grid-cols-[repeat(auto-fill, minmax(300px, 1fr))]">
-            {videos.map((video) => (
-              <VideoGridItem key={video.id} {...video} />
-            ))}
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CategoryPills
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill, minmax(300px, 1fr))]">
+              {videos.map((video) => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 };
 
