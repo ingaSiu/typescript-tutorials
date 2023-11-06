ac87b9bbc3e50158be906af62a196fd43ad96aae
ingaSiu <ingasiud@gmail.com>
2023-11-06 11:29:23 +0200

    fix spelling mistake

--- youtube-home-page/src/components/Sidebar.tsx
+++ youtube-home-page/src/components/Sidebar.tsx
@@ -35,7 +35,7 @@ const Sidebar = () => {
   return (
     <>
       <aside
-        className={`sticky top-0 overflow-y-auto scrollball-hidden pb-4 flex flex-col ml-1 lg:hidden ${
+        className={`sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden ${
           isLargeOpen ? 'lg:hidden' : 'lg:flex'
         }`}
       >
