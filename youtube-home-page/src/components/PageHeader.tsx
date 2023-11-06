b993957e6b31e50f9e4f5f1b27725bc1e708f1b5
ingaSiu <ingasiud@gmail.com>
2023-11-06 11:02:25 +0200

    create a seperate section

--- youtube-home-page/src/components/PageHeader.tsx
+++ youtube-home-page/src/components/PageHeader.tsx
@@ -7,18 +7,10 @@ import { useState } from 'react';
 
 const PageHeader = () => {
   const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
-  const { toggle } = useSidebarContext();
 
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-      <div className={`gap-4 items-center flex-shrink-0 ${showFullWidthSearch ? 'hidden' : 'flex'}`}>
-        <Button onClick={toggle} variant="ghost" size="icon">
-          <Menu />
-        </Button>
-        <a href="/">
-          <img src={logo} className="h-6" />
-        </a>
-      </div>
+      <PageHeaderFirstSection hidden={showFullWidthSearch} />
       <form className={` gap-4 flex-grow justify-center ${showFullWidthSearch ? 'flex' : 'hidden md:flex'}`}>
         {showFullWidthSearch && (
           <Button
@@ -68,3 +60,21 @@ const PageHeader = () => {
 };
 
 export default PageHeader;
+
+type PageHeaderFirstSectionProps = {
+  hidden?: boolean;
+};
+
+export const PageHeaderFirstSection = ({ hidden = false }: PageHeaderFirstSectionProps) => {
+  const { toggle } = useSidebarContext();
+  return (
+    <div className={`gap-4 items-center flex-shrink-0 ${hidden ? 'hidden' : 'flex'}`}>
+      <Button onClick={toggle} variant="ghost" size="icon">
+        <Menu />
+      </Button>
+      <a href="/">
+        <img src={logo} className="h-6" />
+      </a>
+    </div>
+  );
+};
