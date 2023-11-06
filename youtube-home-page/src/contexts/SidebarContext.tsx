6c56bfc9984842f6935745714a1aaf6538ddb9e2
ingaSiu <ingasiud@gmail.com>
2023-11-06 11:17:26 +0200

    fix sidebar resizing

--- youtube-home-page/src/contexts/SidebarContext.tsx
+++ youtube-home-page/src/contexts/SidebarContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useContext, useState } from 'react';
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 
 type SidebarProviderProps = {
   children: ReactNode;
@@ -24,6 +24,18 @@ export const useSidebarContext = () => {
 export const SidebarProvider = ({ children }: SidebarProviderProps) => {
   const [isLargeOpen, setIsLargeOpen] = useState(true);
   const [isSmallOpen, setIsSmallOpen] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (!isScreenSmall()) setIsSmallOpen(false);
+    };
+
+    window.addEventListener('resize', handler);
+
+    return () => {
+      window.removeEventListener('resize', handler);
+    };
+  }, []);
 
   const isScreenSmall = () => {
     return window.innerWidth < 1024;
