28d2882c3484ae3638e5ee060ba337e5e7a2f1a2
ingaSiu <ingasiud@gmail.com>
2023-11-07 16:09:18 +0200

    add some styling

--- take-notes-project/src/App.tsx
+++ take-notes-project/src/App.tsx
@@ -2,17 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { Container } from 'react-bootstrap';
+import NewNote from './components/NewNote';
+
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>}></Route>
-      <Route path="/new" element={<h1>New</h1>}></Route>
-      <Route path="/:id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Edit</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />}></Route>
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route path="/new" element={<NewNote />}></Route>
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </Container>
   );
 };
 
