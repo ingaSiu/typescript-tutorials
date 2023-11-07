b2203186b30094a9d35b261b45749f5339918fc3
ingaSiu <ingasiud@gmail.com>
2023-11-07 15:59:57 +0200

    add routes

--- take-notes-project/src/App.tsx
+++ take-notes-project/src/App.tsx
@@ -1,7 +1,19 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>}></Route>
+      <Route path="/new" element={<h1>New</h1>}></Route>
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />}></Route>
+    </Routes>
+  );
 };
 
 export default App;
