df635f2e23181e76381535448cc388e8a7ae7f49
ingaSiu <ingasiud@gmail.com>
2023-11-08 18:02:33 +0200

    create note list component for home page

---
+++ take-notes-project/src/components/NoteList.tsx
@@ -0,0 +1,35 @@
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
+
+import { Link } from 'react-router-dom';
+
+const NoteList = () => {
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Notes</h1>
+        </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
+export default NoteList;
