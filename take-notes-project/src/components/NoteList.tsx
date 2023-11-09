285aba75243b7120b7fd2e1a7e57bfcae087aba2
ingaSiu <ingasiud@gmail.com>
2023-11-09 09:26:48 +0200

    add simplier select

--- take-notes-project/src/components/NoteList.tsx
+++ take-notes-project/src/components/NoteList.tsx
@@ -1,8 +1,17 @@
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
 
 import { Link } from 'react-router-dom';
+import ReactSelect from 'react-select';
+import { Tag } from '../App';
+import { useState } from 'react';
 
-const NoteList = () => {
+type NoteListProps = {
+  availableTags: Tag[];
+};
+
+const NoteList = ({ availableTags }: NoteListProps) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState('');
   return (
     <>
       <Row>
@@ -23,7 +32,28 @@ const NoteList = () => {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    }),
+                  );
+                }}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
