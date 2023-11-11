4d45452f9b8a506b88ef4c22e4e7d0e89164bd46
ingaSiu <ingasiud@gmail.com>
2023-11-11 07:43:36 +0200

    add delete logic to btn

--- take-notes-project/src/components/Note.tsx
+++ take-notes-project/src/components/Note.tsx
@@ -1,11 +1,15 @@
 import { Badge, Button, Col, Row, Stack } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import { useNote } from './NoteLayout';
 
-const Note = () => {
+type NoteProps = {
+  onDelete: (id: string) => void;
+};
+const Note = ({ onDelete }: NoteProps) => {
   const note = useNote();
+  const navigate = useNavigate();
   return (
     <>
       <Row className="align-items-center mb-4">
@@ -26,7 +30,15 @@ const Note = () => {
             <Link to={`/${note.id}/edit`}>
               <Button variant="primary">Edit</Button>
             </Link>
-            <Button variant="outline-danger">Delete</Button>
+            <Button
+              onClick={() => {
+                onDelete(note.id);
+                navigate('/');
+              }}
+              variant="outline-danger"
+            >
+              Delete
+            </Button>
             <Link to="/">
               <Button variant="outline-secondary">Back</Button>
             </Link>
