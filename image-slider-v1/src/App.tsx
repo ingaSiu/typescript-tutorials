b595f65ab6cbaaafb4332138447ea795f1383f9a
ingaSiu <ingasiud@gmail.com>
2023-11-13 16:14:17 +0200

    add styling

--- image-slider-v1/src/App.tsx
+++ image-slider-v1/src/App.tsx
@@ -8,7 +8,11 @@ import car5 from './imgs/car-5.jpg';
 const IMAGES = [car1, car2, car3, car4, car5];
 
 const App = () => {
-  return <ImageSlider imageUrls={IMAGES} />;
+  return (
+    <div style={{ maxWidth: '1200px', width: '100%', aspectRatio: '10 / 6', margin: '0 auto' }}>
+      <ImageSlider imageUrls={IMAGES} />
+    </div>
+  );
 };
 
 export default App;
--- image-slider-v1/src/components/ImageSlider.tsx
+++ image-slider-v1/src/components/ImageSlider.tsx
@@ -1,3 +1,5 @@
+import './ImageSlider.css';
+
 import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
 
 import { useState } from 'react';
@@ -10,12 +12,12 @@ const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
   const [imageIndex, setImageIndex] = useState(0);
 
   return (
-    <div>
-      <img src={imageUrls[imageIndex]} />
-      <button>
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <img src={imageUrls[imageIndex]} className="img-slider-img" />
+      <button className="img-slider-btn" style={{ left: '0' }}>
         <ArrowBigLeft />
       </button>
-      <button>
+      <button className="img-slider-btn" style={{ right: '0' }}>
         <ArrowBigRight />
       </button>
     </div>
