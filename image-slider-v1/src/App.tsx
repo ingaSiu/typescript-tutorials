5ebe5448e369bdd8bf68b76aadf124aa491d05fd
ingaSiu <ingasiud@gmail.com>
2023-11-13 15:47:17 +0200

    add img arr

--- image-slider-v1/src/App.tsx
+++ image-slider-v1/src/App.tsx
@@ -1,5 +1,14 @@
+import ImageSlider from './components/ImageSlider';
+import car1 from './imgs/car-1.jpg';
+import car2 from './imgs/car-2.jpg';
+import car3 from './imgs/car-3.jpg';
+import car4 from './imgs/car-4.jpg';
+import car5 from './imgs/car-5.jpg';
+
+const IMAGES = [car1, car2, car3, car4, car5];
+
 const App = () => {
-  return <h1>hello</h1>;
+  return <ImageSlider imageUrls={IMAGES} />;
 };
 
 export default App;
