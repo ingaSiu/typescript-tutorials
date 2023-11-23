8918556a4f294367b20a32e45f2052bc022f829b
ingaSiu <ingasiud@gmail.com>
2023-11-23 18:42:41 +0200

    parse theme for this app

--- calculator-v1/src/main.tsx
+++ calculator-v1/src/main.tsx
@@ -1,10 +1,16 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
 import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import theme from './theme.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
