e3cdd8a203618505f6e2425f802a4db5fa6d456f
ingaSiu <ingasiud@gmail.com>
2023-11-24 10:27:15 +0200

    add output container

--- calculator-v1/src/App.tsx
+++ calculator-v1/src/App.tsx
@@ -1,4 +1,13 @@
-import { Container, Paper, styled } from '@mui/material';
+import { Container, Grid, Paper, styled } from '@mui/material';
+
+const OutputContainer = styled('div')(({ theme }) => ({
+  width: '100%',
+  textAlign: 'right',
+  height: '2em',
+  padding: theme.spacing(2),
+  fontSize: '3em',
+  overflow: 'hidden',
+}));
 
 const CalculatorBase = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2),
@@ -9,7 +18,13 @@ const CalculatorBase = styled(Paper)(({ theme }) => ({
 const App = () => {
   return (
     <Container maxWidth="sm">
-      <CalculatorBase elevation={3}>Hi there</CalculatorBase>
+      <CalculatorBase elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <OutputContainer>0</OutputContainer>
+          </Grid>
+        </Grid>
+      </CalculatorBase>
     </Container>
   );
 };
