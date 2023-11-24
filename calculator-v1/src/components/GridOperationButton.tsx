d8907ac035538f46e9f080891c7c4de1a6e68153
ingaSiu <ingasiud@gmail.com>
2023-11-24 10:26:46 +0200

    create operation btn

---
+++ calculator-v1/src/components/GridOperationButton.tsx
@@ -0,0 +1,29 @@
+import { Button, Grid, styled } from '@mui/material';
+
+type GridOperationButtonProps = {
+  operation: string;
+  selectOperation: (operation: string) => void;
+  selectedOperation: string;
+};
+
+const StyledButton = styled(Button)<{ selected: boolean }>((props) => ({
+  backgroundColor: 'rgba(254, 241, 73, 0.1)',
+  borderColor: props.selected ? 'fff' : 'rgba(255, 241, 73, 0.5)',
+}));
+
+const GridOperationButton = ({ operation, selectOperation, selectedOperation }: GridOperationButtonProps) => {
+  return (
+    <Grid item>
+      <StyledButton
+        fullWidth
+        variant="outlined"
+        onClick={() => selectOperation(operation)}
+        selected={selectedOperation === operation}
+      >
+        {operation}
+      </StyledButton>
+    </Grid>
+  );
+};
+
+export default GridOperationButton;
