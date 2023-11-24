fa1e133317321ddcb8e7ba2938f0377ed100c0a6
ingaSiu <ingasiud@gmail.com>
2023-11-24 09:58:38 +0200

    create a content box with mui

--- calculator-v1/src/App.tsx
+++ calculator-v1/src/App.tsx
@@ -1,5 +1,17 @@
+import { Container, Paper, styled } from '@mui/material';
+
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius: 15,
+}));
+
 const App = () => {
-  return <h1>Calculator App</h1>;
+  return (
+    <Container maxWidth="sm">
+      <CalculatorBase elevation={3}>Hi there</CalculatorBase>
+    </Container>
+  );
 };
 
 export default App;
