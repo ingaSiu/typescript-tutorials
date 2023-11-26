d564c9636b16a0a2ef37f4972e13d532a4cbd912
ingaSiu <ingasiud@gmail.com>
2023-11-26 13:15:49 +0200

    add tests for buttons

--- calculator-v1/src/__tests__/App.test.tsx
+++ calculator-v1/src/__tests__/App.test.tsx
@@ -19,5 +19,21 @@ describe('Calculator App tests', () => {
 
     const output = screen.getByTestId('output');
     expect(output).toHaveTextContent('0');
+
+    const buttons = screen.getAllByRole('button');
+
+    expect(buttons.length).toBe(19);
+
+    for (let i = 0; i < 10; i++) {
+      expect(screen.getByRole('button', { name: `${i}` })).toBeInTheDocument();
+    }
+    expect(screen.getByRole('button', { name: 'AC' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'C' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '*' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '÷' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '+' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '-' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '.' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '=' })).toBeInTheDocument();
   });
 });
